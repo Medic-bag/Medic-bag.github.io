18d7e5bdab5c329a18c5b3d0614be5aa6151fb9c
Medic-bag <rileygirling@gmail.com>
2024-10-11 10:06:20 -0600

    fixed bubble demo and made some progress on the array project

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -4,22 +4,72 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-// make guitar her idiot
+// make guitar hero idiot
+
+let balls = [];
 
 function setup() {
   createCanvas(400, windowHeight);
-  makeCircles();
+
+  // window.setInterval(spawnBall, 500)
 }
 
 function draw() {
   background(220);
-  circle(fartLeftCircle.x, fartLeftCircle.y, fartLeftCircle.radius);
+  makeCircles();
+  
+  
 }
 
 function makeCircles() {
-  let fartLeftCircle = {
-    x: 100 - 20,
+  let farLeftCircle = {
+    x: 80,
     y: windowHeight - 100,
     radius: 40,
   };
+  circle(farLeftCircle.x, farLeftCircle.y, farLeftCircle.radius);
+  
+  let farRightCircle = {
+    x: 320,
+    y: windowHeight - 100,
+    radius: 40,
+  };
+  circle(farRightCircle.x, farRightCircle.y, farRightCircle.radius);
+
+  let RightCircle = {
+    x: 240,
+    y: windowHeight - 100,
+    radius: 40,
+  };
+  circle(RightCircle.x, RightCircle.y, RightCircle.radius);
+
+  let LeftCircle = {
+    x: 160,
+    y: windowHeight - 100,
+    radius: 40,
+  };
+  circle(LeftCircle.x, LeftCircle.y, LeftCircle.radius);
+}
+
+function spawnBall() {
+  let someBall = {
+    x: randBallX(),
+    y: windowHeight + 30,
+  };
+}
+
+function randBallX() {
+  let rndNum = random(0, 4);
+  if (rndNum <=1) {
+    return 80;
+  }
+  else if (rndNum <=2) {
+    return 160;
+  }
+  else if (rndNum <= 3) {
+    return 240;
+  }
+  else {
+    return 320;
+  }
 }
--- bubbles/sketch.js
+++ bubbles/sketch.js
@@ -1,28 +1,84 @@
-// Bubble array object notation
-// Riley Sane
-// 10/10/2024
-//
-// - Removing objects from an array
+
+// Bubble Array Object Notation Demo
+// Removing objects from the array
 
 let theBubbles = [];
+let deathLocations = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noStroke();
-  for (let i = 0; i < 10000000; i++) {
-    spawnBubbles();
+
+  for (let i = 0; i < 5; i++) {
+    spawnBubble();
   }
+
+  //create a new bubble every half second
+  window.setInterval(spawnBubble, 500);
 }
 
 function draw() {
   background(220);
-  moveBubblesRandomly();
+  // moveBubblesRandomly();
+  moveBubblesWithNoise();
   displayBubbles();
+  displayDeathSpots();
 }
+
+function mousePressed() {
+  for (let bubble of theBubbles) {
+    if (clickedInBubble(mouseX, mouseY, bubble)) {
+      let theIndex = theBubbles.indexOf(bubble);
+      theBubbles.splice(theIndex, 1);
+      addDeath(mouseX, mouseY);
+    }
+  }
+}
+
+function displayDeathSpots() {
+  for (let spot of deathLocations) {
+    textAlign(CENTER, CENTER);
+    fill("black");
+    text("X", spot.x, spot.y);
+  }
+}
+
+function addDeath(_x, _y) {
+  let deathSpot = {
+    x: _x, 
+    y: _y,
+  };
+  deathLocations.push(deathSpot);
+}
+
+function clickedInBubble(x, y, theBubble) {
+  let distanceAway = dist(x, y, theBubble.x, theBubble.y);
+  if (distanceAway < theBubble.radius) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 
 function displayBubbles() {
   for (let bubble of theBubbles) {
-    fill(bubble.r, bubble.g, bubble.b);
-    circle(bubble.x, bubble.y, bubble.size);
+    fill(bubble.r, bubble.g, bubble.b, bubble.alpha);
+    circle(bubble.x, bubble.y, bubble.radius * 2);
+  }
+}
+
+function moveBubblesWithNoise() {
+  for (let bubble of theBubbles) {
+    let x = noise(bubble.timeX) * width;
+    let y = noise(bubble.timeY) * height;
+
+    bubble.x = x;
+    bubble.y = y;
+
+    bubble.timeX += bubble.deltaTime;
+    bubble.timeY += bubble.deltaTime;
   }
 }
 
@@ -30,32 +86,37 @@ function moveBubblesRandomly() {
   for (let bubble of theBubbles) {
     let choice = random(100);
     if (choice < 50) {
-      // move up
+      //move up
       bubble.y -= bubble.speed;
     }
     else if (choice < 65) {
-      // move down
+      //move down
       bubble.y += bubble.speed;
     }
     else if (choice < 80) {
-      // move right
+      //move right
       bubble.x += bubble.speed;
     }
     else {
+      //move left
       bubble.x -= bubble.speed;
     }
   }
 }
 
-function spawnBubbles() {
+function spawnBubble() {
   let someBubble = {
     x: random(width),
     y: height + random(0, 25),
-    speed: random(2,5),
-    size: random(20, 40),
+    speed: random(2, 5),
+    radius: random(20, 40),
     r: random(255),
     g: random(255),
     b: random(255),
+    alpha: random(255),
+    timeX: random(10000000),
+    timeY: random(10000000),
+    deltaTime: 0.002,
   };
   theBubbles.push(someBubble);
 }
